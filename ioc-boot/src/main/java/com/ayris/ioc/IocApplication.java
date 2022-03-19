c3fc70f86bdfb3a72b17a6ad4be55fe7af874842
ayris <nleisure@163.com>
2022-03-19 21:59:50 +0800

    排除Bean加载

--- ioc-boot/src/main/java/com/ayris/ioc/IocApplication.java
+++ ioc-boot/src/main/java/com/ayris/ioc/IocApplication.java
@@ -1,8 +1,24 @@
 package com.ayris.ioc;
 
+import com.ayris.ioc.compent.TestBean2;
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.AutoConfigurationExcludeFilter;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.TypeExcludeFilter;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.FilterType;
 
+@ComponentScan(basePackages = "com.ayris.ioc", excludeFilters = {@ComponentScan.Filter(
+        type = FilterType.CUSTOM,
+        classes = {TypeExcludeFilter.class}
+), @ComponentScan.Filter(
+        type = FilterType.CUSTOM,
+        classes = {AutoConfigurationExcludeFilter.class}
+), @ComponentScan.Filter(
+        type = FilterType.ASSIGNABLE_TYPE,
+        classes = {TestBean2.class}
+)
+})
 @SpringBootApplication
 public class IocApplication {
 
---
+++ ioc-boot/src/main/java/com/ayris/ioc/compent/TestBean2.java
@@ -0,0 +1,11 @@
+package com.ayris.ioc.compent;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class TestBean2 {
+
+    public TestBean2() {
+        System.out.println("TestBean2 created---");
+    }
+}
